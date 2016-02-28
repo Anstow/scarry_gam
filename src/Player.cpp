5fd62e5d5af3ce886bdde06985d6445e075c1cc9
anstow <dvd_anstow@hotmail.co.uk>
2016-02-28 14:53:10 +0000

    Smoother movement.

--- src/Player.cpp
+++ src/Player.cpp
@@ -2,31 +2,43 @@
 
 #include <iostream>
 
-constexpr double Player::speed_;
+constexpr double Player::minSpeed_;
+constexpr double Player::accel_;
+constexpr double Player::damping_;
+constexpr double Player::time_;
+
 Player::Player(tank::Controller const& c, Vertex vert_id)
     : controller_ {c}
     , currentNode_ {vert_id}
 {
+    auto& pos = getPos();
+    sprite_.setPosition(pos.x, pos.y);
 }
 
 void Player::update()
 {
     constexpr float dead_zone = 0.2; // inverse pad sensitivity [0,1]
 
-    auto& pos = getPos();
-
     tank::Vectorf ls = controller_.leftStick();
     float magnitude = ls.magnitude();
+    tank::Vectorf disp = {0,0};
     if (magnitude > dead_zone) {
-        const auto disp = ls - (dead_zone * (ls / magnitude)) / (1 - dead_zone);
-
-        pos += speed_ * disp;
+        disp = ls - (dead_zone * (ls / magnitude)) / (1 - dead_zone);
     }
-
-    sprite_.setPosition(pos.x, pos.y);
+    moveBy(disp);
 }
 
 void Player::draw(sf::RenderTarget& target) const
 {
     target.draw(sprite_);
+}
+
+void Player::moveBy(tank::Vectorf const& disp) {
+
+    auto& pos = getPos();
+    pos += vel_ * time_  + disp * accel_ * time_ * time_ / 2;
+    vel_ *= damping_;
+    vel_ += disp * accel_ * time_;
+
+    sprite_.setPosition(pos.x, pos.y);
 }
--- src/Player.hpp
+++ src/Player.hpp
@@ -12,13 +12,20 @@ class Player : public Entity
     sf::CircleShape sprite_ {20};
     Vertex currentNode_;
 
-    static constexpr double speed_ = 2;
+    tank::Vectorf vel_{0,0};
+
+    static constexpr double minSpeed_ = 0.001;
+    static constexpr double accel_ = 3.0;
+    static constexpr double damping_ = 0.9;
+    static constexpr double time_ = 0.25;
 
 public:
     Player(tank::Controller const& c, Vertex startNode_);
 
     void update() override;
     void draw(sf::RenderTarget&) const override;
+
+    void moveBy(tank::Vectorf const& disp);
     
     Vertex const& getCurrentNode() const { return currentNode_; }
 };
